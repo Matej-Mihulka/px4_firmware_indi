07fcbd2b9e6bc189aaf1f3fb7d97edf6997c077e
mihulmat <mihulmat@fel.cvut.cz>
2026-06-18 22:13:24 +0200

    Initial commit, basic setup

---
+++ src/modules/indi_controller/indi_controller.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <px4_platform_common/module.h>
+#include <px4_platform_common/module_params.h>
+#include <uORB/Subscription.hpp>
+#include <uORB/Publication.hpp>
+#include <uORB/topics/debug_key_value.h>
+
+
+// this is so it is compiled such that OS written in C can understand it
+extern "C" __EXPORT int indi_controller_main(int argc, char *argv[]);
+
+// inherit stuff from ModuleBase
+class IndiController : public ModuleBase<IndiController>, public ModuleParams
+{
+public:
+	IndiController();
+	~IndiController() override = default;
+
+	static int task_spawn(int argc, char *argv[]);
+
+	static IndiController *instantiate(int argc, char *argv[]);
+
+	static int custom_command(int argc, char *argv[]);
+
+	static int print_usage(const char *reason = nullptr);
+
+	void Run() override;
+
+private:
+	// listening to MAVROS/ROS
+	uORB::Subscription _debug_key_value_sub{ORB_ID(debug_key_value)};
+
+	uORB::Publication<_debug_key_value_s> _debug_key_value_pub{ORB_ID(debug_key_value)};
+
+	int
+}
+
